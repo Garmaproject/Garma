56216d8091d5c58349e79ab6e76b563637fed628
Garmaproject <gnoriegac@unican.es>
2025-01-15 12:56:37 +0100

    Update Database_code.sql

--- Database_code.sql
+++ Database_code.sql
@@ -5,6 +5,7 @@ CREATE SCHEMA example_schema;
 CREATE EXTENSION postgis;
 
 -- Create a sequence called 'user_seq' in the 'example_schema' schema
+
 CREATE SEQUENCE example_schema.user_seq
     START WITH 1          -- Start at 1
     INCREMENT BY 1        -- Increment by 1
@@ -14,12 +15,13 @@ CREATE SEQUENCE example_schema.user_seq
 
 -- Create a table 'users' in the 'example_schema' schema
 -- with a geometry column and using the sequence for the id column
+
 CREATE TABLE example_schema.users (
     id INTEGER DEFAULT nextval('example_schema.user_seq') PRIMARY KEY, -- Unique identifier using the sequence
-    geom GEOMETRY(Point, 4326),          -- Geometry column of type Point with SRID 4326
+    geom GEOMETRY(Point, 4326),          -- Geometry column of type Point with SRID 4326 (Example)
     element VARCHAR(100) NOT NULL,       -- Element name, not null
-    physical_conservation VARCHAR(100) UNIQUE NOT NULL, -- Unique and not null physical conservation
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Registration date, default to the current time
+    physical_conservation VARCHAR(100) UNIQUE NOT NULL, -- Unique and not null physical conservation (Example)
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Registration date, default to the current time (Example)
 );  --and so on
 
 -- Create a spatial index on the geom column
